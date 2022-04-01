d746deae9382dd4f82a76d6b83ae6d85ab56b0c5
dekh0 <66974623+dekh0@users.noreply.github.com>
2022-04-01 11:41:35 +0400

    process

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import './App.css';
 import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
 import NavBar from './components/Navbar/NavBar';
+import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <NavBar />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className="wrapper">
+        <Header />
+        <NavBar />
+        <div className="wrapper-profile">
+          <Routes>
+            <Route path='/dialogs' element={<Dialogs/>} />
+            <Route path='/profile' element={<Profile/>} />
+            <Route path='/news,' element={<News/>} />
+            <Route path='/music' element={<Music/>} />
+            <Route path='/settings' element={<Settings/>} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
